df7cb51f9ba45d9833a97e52120585f83bbddeab
kannkyo <15080890+kannkyo@users.noreply.github.com>
2024-11-17 00:40:33 +0900

    wip: translate

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 <p align="center"><a href="" target="_blank" rel="noreferrer noopener"><img width="350" alt="designr logo" src="https://github.com/ANG13T/aerospace-cybersecurity-tools/blob/main/src/assets/images/globe.png"></a></p>
 
-<h1 align="center">Aerospace Hacking Tools</h1>
-<p align="center">A comprehensive collection of space-related cybersecurity tools</p>
+<h1 align="center">航空宇宙ハッキングツール</h1>
+<p align="center">宇宙関連のサイバーセキュリティツールの包括的なコレクション</p>
 
 <p align="center"><a rel="noreferrer noopener" href="https://tools.g4lxy.space/"><img alt="Chrome Web Store" src="https://img.shields.io/badge/VISIT WEBSITE-141e24.svg?&style=for-the-badge"></a>  
 
-
 # Contributing
 
-Feel free to contribute if you would like to add a tool or implement any features or improvements to the site. 
+Feel free to contribute if you would like to add a tool or implement any features or improvements to the site.
 Please make sure to reference the [contributing document](https://github.com/ANG13T/aerospace-hacking-tools/blob/main/CONTRIBUTING.md) before submitting a pull request.
--- src/data/projects.js
+++ src/data/projects.js
@@ -3,15 +3,15 @@ const projects = [
   {
     name: "CCSDSPy",
     dateAdded: "6/16/2023",
-    tags: ["Satellites", "OSINT"],
-    description: "A Python package for reading CCSDS packet data.",
+    tags: ["衛星", "OSINT"],
+    description: "CCSDS パケット データを読み取るための Python パッケージ。",
     website: "https://docs.ccsdspy.org/en/latest/",
     image: "ccsdspy.png"
   },
   {
     name: "Satpy",
     dateAdded: "6/21/2023",
-    tags: ["Satellites", "OSINT"],
+    tags: ["衛星", "OSINT"],
     description: "Python library for obtaining remote sensing data and writing it to various file formats.",
     website: "https://github.com/pytroll/satpy",
     image: "satpy.png"
@@ -19,15 +19,15 @@ const projects = [
   {
     name: "Python SGP4",
     dateAdded: "6/16/2023",
-    tags: ["Satellites"],
-    description: "Python version of the SGP4 satellite position library.",
+    tags: ["衛星"],
+    description: "SGP4 衛星位置ライブラリの Python バージョン。",
     website: "https://github.com/brandon-rhodes/python-sgp4",
     image: "python-sgp4.png"
   },
   {
     name: "poliastro",
     dateAdded: "6/21/2023",
-    tags: ["Astrodynamics"],
+    tags: ["天体力学"],
     description: "Python library for interactive Astrodynamics and Orbital Mechanics.",
     website: "https://docs.poliastro.space/en/stable/",
     image: "poliastro.png"
@@ -35,16 +35,16 @@ const projects = [
   {
     name: "Skyfield",
     dateAdded: "6/21/2023",
-    tags: ["Astrodynamics", "Astronomy"],
-    description: "Skyfield computes positions for the stars, planets, and satellites in orbit around the Earth.",
+    tags: ["天体力学", "天文学"],
+    description: "Skyfield は、地球の周りを周回する恒星、惑星、衛星の位置を計算します。",
     website: "https://rhodesmill.org/skyfield/",
     image: "skyfield.png"
   },
   {
     name: "satellite.js",
     dateAdded: "6/21/2023",
-    tags: ["Satellites"],
-    description: "Modular set of functions for SGP4 and SDP4 propagation of TLEs.",
+    tags: ["衛星"],
+    description: "TLE の SGP4 および SDP4 伝播用のモジュール式関数セット。",
     website: "https://github.com/shashwatak/satellite-js",
     image: "satellite-js.png"
   },
@@ -52,30 +52,30 @@ const projects = [
     name: "Cesium",
     dateAdded: "6/21/2023",
     tags: ["Earth", "OSINT"],
-    description: "Cesium is the open platform for software applications designed to unleash the power of 3D data.",
+    description: "Cesium は、3D データのパワーを最大限に引き出すように設計されたソフトウェア アプリケーション用のオープン プラットフォームです。",
     website: "https://github.com/CesiumGS/cesium",
     image: "cesium.png"
   },
   {
     name: "SOLM",
     dateAdded: "6/21/2023",
-    tags: ["Satellites"],
-    description: "Satellite Operations Language Meta-model (SOLM) for representing spacecraft operations procedures.",
+    tags: ["衛星"],
+    description: "宇宙船の運用手順を表す衛星運用言語メタモデル (SOLM)。",
     website: "https://www.omg.org/spec/SOLM/About-SOLM/",
     image: "SOLM.png"
   },
   {
     name: "OpenTsiolkovsky",
     dateAdded: "6/21/2023",
-    tags: ["Spacecraft", "Simulation"],
-    description: "Rocket Flight Simulator.",
+    tags: ["宇宙機", "シミュレーション"],
+    description: "ロケット飛行シミュレーター。",
     website: "https://github.com/istellartech/OpenTsiolkovsky",
     image: "opent.png"
   },
   {
     name: "ISS Docking Simulator",
     dateAdded: "6/21/2023",
-    tags: ["Spacecraft", "Simulation"],
+    tags: ["宇宙機", "シミュレーション"],
     description: "Simulator with the controls of used by NASA Astronauts to  pilot the SpaceX Dragon 2 vehicle to the ISS.",
     website: "https://iss-sim.spacex.com/",
     image: "iss-docking-sim.jpeg"
@@ -83,7 +83,7 @@ const projects = [
   {
     name: "SatDump",
     dateAdded: "6/21/2023",
-    tags: ["Radio", "Satellites"],
+    tags: ["Radio", "衛星"],
     description: "A generic satellite data processing software.",
     website: "https://github.com/SatDump/SatDump",
     image: "satdump.png"
@@ -91,7 +91,7 @@ const projects = [
   {
     name: "SatIntel",
     dateAdded: "6/28/2023",
-    tags: ["Satellites"],
+    tags: ["衛星"],
     description: "OSINT tool for Satellites 🛰. Extract satellite telemetry, receive orbital predictions, and parse TLEs 🔭",
     website: "https://github.com/ANG13T/SatIntel",
     image: "satintel.png"
@@ -99,7 +99,7 @@ const projects = [
   {
     name: "SpaceXTelemetry API",
     dateAdded: "6/28/2023",
-    tags: ["Spacecraft", "Launches"],
+    tags: ["宇宙機", "打ち上げ"],
     description: "Open Source REST API for telemetry and predictions to rocket launches",
     website: "https://github.com/R4yGM/SpaceXTelemetry-Api",
     image: "spacextelemetry.png"
@@ -107,7 +107,7 @@ const projects = [
   {
     name: "Astrocats",
     dateAdded: "6/28/2023",
-    tags: ["Planets", "Astronomy"],
+    tags: ["Planets", "天文学"],
     description: "Open Astronomy Catalogs",
     website: "https://astrocats.space/",
     image: "astrocats.png"
@@ -115,7 +115,7 @@ const projects = [
   {
     name: "Astrometry.net",
     dateAdded: "6/28/2023",
-    tags: ["Star Trackers", "Astronomy"],
+    tags: ["Star Trackers", "天文学"],
     description: "Astrometric calibration meta-data tool for astronomical imaging",
     website: "http://astrometry.net/",
     image: "astrometry.png"
@@ -123,7 +123,7 @@ const projects = [
   {
     name: "Virgo",
     dateAdded: "6/28/2023",
-    tags: ["Radio", "Astronomy"],
+    tags: ["Radio", "天文学"],
     description: "A Versatile Spectrometer for Radio Astronomy",
     website: "https://github.com/0xCoto/VIRGO",
     image: "virgo.png"
@@ -131,7 +131,7 @@ const projects = [
   {
     name: "100,000 Stars",
     dateAdded: "6/28/2023",
-    tags: ["Star Trackers", "Astronomy"],
+    tags: ["Star Trackers", "天文学"],
     description: "Interactive Visualization of the Stellar Neighborhood showing the location of 119,617 nearby stars",
     website: "https://stars.chromeexperiments.com/",
     image: "100000Stars.png"
@@ -139,7 +139,7 @@ const projects = [
   {
     name: "Harmony of the Spheres",
     dateAdded: "6/28/2023",
-    tags: ["Simulation", "Astrodynamics"],
+    tags: ["シミュレーション", "天体力学"],
     description: "Newtonian n-body gravity simulator",
     website: "https://gravitysimulator.org/",
     image: "harmony.png"
@@ -147,7 +147,7 @@ const projects = [
   {
     name: "KStars",
     dateAdded: "6/28/2023",
-    tags: ["Simulation", "Astronomy"],
+    tags: ["シミュレーション", "天文学"],
     description: "Feature-rich and free astronomy software",
     website: "https://kstars.kde.org/",
     image: "kstars.png"
@@ -155,7 +155,7 @@ const projects = [
   {
     name: "Mars Now",
     dateAdded: "6/28/2023",
-    tags: ["Simulation", "Planets"],
+    tags: ["シミュレーション", "Planets"],
     description: "Visualization of all data transmitted by spacecraft on Mars via NASA’s Mars Relay Network",
     website: "https://mars.nasa.gov/explore/mars-now/",
     image: "marsnow.png"
@@ -171,7 +171,7 @@ const projects = [
   {
     name: "Orbit Simulator",
     dateAdded: "6/28/2023",
-    tags: ["Astrodynamics", "Planets"],
+    tags: ["天体力学", "Planets"],
     description: "Powerful numerical integration tool for n-body gravitational simulations",
     website: "http://orbitsimulator.com/gsim.html",
     image: "orbitsim.png"
@@ -179,7 +179,7 @@ const projects = [
   {
     name: "MMSSTV",
     dateAdded: "7/01/2023",
-    tags: ["Satellites", "Radio"],
+    tags: ["衛星", "Radio"],
     description: "This program is for transmitting and receiving SSTV using a PC soundcard.",
     website: "https://hamsoft.ca/pages/mmsstv.php",
     image: "mmsstv.jpg"
@@ -187,7 +187,7 @@ const projects = [
   {
     name: "Orbitron - Satellite Tracking System",
     dateAdded: "7/02/2023",
-    tags: ["Satellites", "OSINT", "Radio"],
+    tags: ["衛星", "OSINT", "Radio"],
     description: "Orbitron is a satellite tracking system for radio amateur and observing purposes.",
     website: "http://www.stoff.pl/",
     image: "orbitron.jpg"
@@ -195,7 +195,7 @@ const projects = [
   {
     name: "Stellarium",
     dateAdded: "07/09/2023",
-    tags: ["Astronomy", "Star Trackers"],
+    tags: ["天文学", "Star Trackers"],
     description: "Desktop planetarium software that shows a realistic sky in 3D.",
     website: "https://github.com/Stellarium/stellarium",
     image: "stellarium.jpg"
@@ -203,7 +203,7 @@ const projects = [
   {
     name: "SatNOGS",
     dateAdded: "7/20/2023",
-    tags: ["Satellites", "Ground Stations"],
+    tags: ["衛星", "Ground Stations"],
     description: "Open Source global network of satellite ground-stations",
     website: "https://satnogs.org/",
     image: "SatNOGS.png"
@@ -219,7 +219,7 @@ const projects = [
   {
     name: "TinyGS",
     dateAdded: "10/15/2023",
-    tags: ["Satellites", "Ground Stations"],
+    tags: ["衛星", "Ground Stations"],
     description: "TinyGS is an open network of Ground Stations distributed around the world to receive and operate LoRa satellites, weather probes and other flying objects, using cheap and versatile modules.",
     website: "https://tinygs.com/",
     image: "tinygs.png"
@@ -227,7 +227,7 @@ const projects = [
   {
     name: "STK",
     dateAdded: "06/23/2024",
-    tags: ["Mission Design", "Simulation"],
+    tags: ["Mission Design", "シミュレーション"],
     description: "Ansys Systems Tool Kit (STK) provides a physics-based modeling environment for analyzing platforms and payloads in a realistic mission context.",
     website: "https://www.ansys.com/products/missions/ansys-stk",
     image: "stk.png"
@@ -236,23 +236,23 @@ const projects = [
     name: "Open MCT",
     dateAdded: "06/23/2024",
     tags: ["Ground Control"],
-    description: "Open MCT is a next-generation mission operations data visualization framework. Web-based, for desktop and mobile.",
+    description: "Open MCT は、次世代のミッション オペレーション データ視覚化フレームワークです。Web ベースで、デスクトップとモバイルに対応しています。",
     website: "https://nasa.github.io/openmct/",
     image: "openmct.png"
   },
   {
     name: "satvis.space",
     dateAdded: "06/23/2024",
-    tags: ["OSINT", "Satellites"],
-    description: "Satellite orbit visualization and pass prediction using satellite.js and CesiumJS.",
+    tags: ["OSINT", "衛星"],
+    description: "satellite.jsとCesiumJSを使った衛星軌道の可視化と通過予測を提供します。",
     website: "https://github.com/Flowm/satvis",
     image: "satvis.png"
   },
   {
     name: "CCSDS WIRESHARK DISSECTOR",
     dateAdded: "06/23/2024",
-    tags: ["Satellites", "OSINT"],
-    description: "this project extends the list by providing dissectors for CCSDS protocols.",
+    tags: ["衛星", "OSINT"],
+    description: "このプロジェクトは、CCSDS プロトコルのディセクタを提供することでリストを拡張します。",
     website: "https://essr.esa.int/project/ccsds-wireshark-dissector",
     image: "ccsds-wireshark-dissector.png"
   },
